b19f8b382e1789b8d55204645ac46213e6f79300
Mahyad <saedpanahmahyad@gmail.com>
2023-12-03 22:51:16 +0330

    create-Products-class

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+import Products from "./models/Products.js";
 import { fetchData } from "./utils/httpReq.js";
+
+const productsNode = document.getElementById("products");
 
 async function render() {
   const productsData = await fetchData();
-  console.log(productsData);
+  const productsInstance = new Products(productsNode, productsData);
+
+  productsInstance.showProducts();
 }
 
 document.addEventListener("DOMContentLoaded", render);
